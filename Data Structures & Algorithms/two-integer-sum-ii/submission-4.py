dd6154215e917be01bc22626cfccf3ca96754f9d
Sato0123 <136211768+Sato0123@users.noreply.github.com>
2026-07-28 22:55:22 +0900

    Add: two-integer-sum-ii - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-4.py
@@ -0,0 +1,13 @@
+from typing import List
+
+# O(N^2)
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        n = len(numbers)
+        for i in range(n):
+            for j in range(n):
+                if i == j:
+                    continue
+                if numbers[i] + numbers[j] == target:
+                    return [i + 1, j + 1]
+
